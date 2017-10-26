2fbca6047869c6f5dd32dd144602eaea4be35f30
Gulaabihaathee <32573906+Gulaabihaathee@users.noreply.github.com>
2017-10-26 09:10:31 +0200

    balls.py
    Combinations of Cross Elements

--- balls.py
+++ balls.py
@@ -10,6 +10,29 @@ def Ones_Counter(GameBoard):
 		OAPOSij.append(np.sum(GameBoard[i][j:]) + np.sum(GameBoard.transpose()[j][i+1:]))
 	return(OAPOSij)
 
+#Permutations Generator
+def CrossCombine(St_Pos_List):
+	#Knot positions	
+	Pos_Moves = [St_Pos_List]
+	Comb_Vec = [Pos_Moves]
+	print("CV: ",Comb_Vec)
+	#Vertical
+	for i in range(St_Pos_List[0]+1, n):
+		Pos_Moves.append([i, St_Pos_List[1]])
+		print("PM: ",Pos_Moves)
+		#Comb_Vec.extend([Pos_Moves])
+		#print("CV: ",Comb_Vec)
+	#Horizontal
+	Pos_Moves = [St_Pos_List]
+	for j in range(St_Pos_List[1]+1, n):
+		Pos_Moves.append([St_Pos_List[0],j])
+		print("PM: ",Pos_Moves)
+		#Comb_Vec.extend([Pos_Moves])
+		#print("CV: ",Comb_Vec)
+	#Return Cross elements from Knot position
+	return(Pos_Moves)
+	#return(Pos_Moves)
+
 #Algorithm Starting Positions
 Starting_Positions = []
 
@@ -19,7 +42,8 @@ GameBoard = np.array([[1]*n]*n)
 #Algorithm Starting Positions Generator 
 for i in range(0,n):
 	for j in range(0,n):
-		Starting_Positions.append((i,j))
+		Starting_Positions.append([i,j])
 
 #All Possible Moves Amount for current GameBoard
 PMA = np.sum(Ones_Counter(GameBoard))
+
